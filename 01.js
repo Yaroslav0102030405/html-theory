bc68686010c5919bae98837ee70d3c617227c69e
Yaroslav0102030405 <you@example.com>
2025-01-30 21:57:15 +0200

    lesson JS

---
+++ 01.js
@@ -0,0 +1,119 @@
+// Зміни const і let
+
+// number - ціді та дрібні числа
+const age = 10;
+const totalPrice = 200.14;
+// string
+const message = 'Добро пожаловать!';
+// boolean
+const isOpen = true;
+// null
+const ng = null;
+// undefined
+let a;
+// силочні типи об'ект
+const array = [];
+const ob = {};
+
+// оператор typeof для перевірки зміної
+console.log(typeof a);
+console.log(2);
+
+// методи для роботи з числами
+// 1 метод консуктор Number - который приводит строку к числу или если не получилось привести повертає NaN;
+const ab = '5';
+console.log(Number(ab));
+// 2 Number.parseInt() - парсіт ціле число
+const elementWidth = '59px';
+const elementWidthParse = Number.parseInt(elementWidth);
+console.log(elementWidthParse);
+// 2 Number.parseFloat() - парсіт дробне число
+const elementHeight = '200.12px';
+const elementHeightParse = Number.parseFloat(elementHeight);
+console.log(elementHeightParse);
+// 3 toFixed()
+const salary = 13.214343434;
+const salaryNumber = Number(salary.toFixed(5));
+console.log(salaryNumber);
+
+// Приклоди з обёектом Math
+
+// Приклад
+const colors = ['orange', 'tomato', 'blue', 'green', 'gray', 'black'];
+
+const max = colors.length - 1;
+const min = 0;
+const index = Math.round(Math.random() * (max - min) + min);
+const color = colors[index];
+
+document.body.style.backgroundColor = color;
+
+// 2 Методи роботи з строками
+// Довжина строки
+const message2 = 'В этой строке 26 символов.';
+console.log(message2.length);
+// Дізнатися останій символ -1
+console.log(message2.length - 1);
+
+// Конкатенація строк
+const firsName = 'Chelsy';
+const lastName = 'Ema';
+const fullName = firsName + ' ' + lastName;
+console.log(fullName);
+
+// Шаблоні строкі
+const quantity = 5;
+const orderMsg = `Ви замовили ${quantity} холодильників`;
+console.log(orderMsg);
+
+// нормалізація користувацього вводу
+const brand = 'Samsung';
+// const inDb = 'samsung';
+const normalizedBrand = brand.toLowerCase();
+console.log(normalizedBrand);
+
+// копія
+const brandCopy = brand.slice(3);
+console.log(brandCopy);
+
+// Пошук в строке под строку з методом includes()
+const word1 = 'cпам';
+const string1 = 'Я принц Абудала, и это не Cпам';
+
+const normalizedString = string1.toLowerCase().includes(word1);
+console.log(normalizedString);
+
+// оператори порівняння
+
+// Конструктор Boolean()
+// 6 ложних значень які приводяться до false а все остальне приводиться до true
+// 0, NaN, null, undefined, порожня строка "", false
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(''));
+console.log(Boolean(false));
+
+console.log(Boolean('Привіт'));
+
+//
+const sub = 'pro';
+
+const canAccessContent = sub === 'pro' || sub === 'vip';
+console.log(canAccessContent);
+
+//
+const balance = 1000;
+let message3;
+
+if (balance >= 0) {
+  message3 = 'Позитивний баланс';
+} else {
+  message3 = 'Негативний баланс';
+}
+console.log(message3);
+
+// тернарний оператор
+const message4 = balance >= 1000 ? 'Позитивний' : 'Негативний';
+console.log(message4);
