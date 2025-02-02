dcebd7f47fac88c95c04ec9fa53a20495e95054c
Yaroslav0102030405 <you@example.com>
2025-02-02 16:17:37 +0200

    les-3

--- 01.js
+++ 01.js
@@ -236,4 +236,118 @@ friends.push('Kiwi');
 friends.unshift('Aaa');
 friends.pop();
 console.log(friends);
-console.log(friends.length - 1);
+// console.log(friends.length - 1);
+
+const friends1 = ['Mango', 'Kiwi', 'Ajax', 'Poly'];
+
+for (let i = 0; i < friends1.length; i += 1) {
+  console.log(`${i + 1} - ${friends1[i]}`);
+  // console.log(`${friends1[i]}`);
+}
+
+// for (const friend of friends1) {
+//   console.log(friend);
+// }
+
+const numbers = [10, 20, 30, 2, 3, 4, 5];
+let totalNumbers = 0;
+
+for (const number of numbers) {
+  // totalNumbers += number;
+
+  if (number % 2 === 0) {
+    totalNumbers += number;
+  }
+}
+console.log(totalNumbers);
+
+const logins = ['a', 'b', 'c'];
+const logiToFind = 'b';
+// let message8 = '';
+
+// for (const login of logins) {
+//   if (logiToFind === login) {
+//     message8 = `Знайшли ${logiToFind}`;
+//     break;
+//   }
+
+//   message8 = `Не знайли ${logiToFind}`;
+// }
+
+const message8 = logins.includes(logiToFind)
+  ? `Користувач ${logiToFind} знайден`
+  : `Користувач ${logiToFind} не знайден`;
+console.log(message8);
+
+const numbers1 = [5, 4, 3, , 2, 1];
+let smallestNumbers = numbers1[0];
+
+for (const number of numbers1) {
+  if (number < smallestNumbers) {
+    smallestNumbers = number;
+  }
+}
+console.log(smallestNumbers);
+
+const friends2 = ['Mango', 'Kiwi', 'Ajax', 'Poly'];
+const string = friends2.join(', ');
+
+console.log(string);
+
+const string3 = 'JavaScript';
+const letters = string3.split('');
+let invertedString = '';
+
+for (const letter of letters) {
+  // console.log(letter);
+  // if (letter === letter.toLowerCase()) {
+  //   invertedString += letter.toUpperCase();
+  // } else {
+  //   invertedString += letter.toLowerCase();
+  // }
+
+  invertedString +=
+    letter === letter.toLowerCase()
+      ? letter.toUpperCase()
+      : letter.toLowerCase();
+}
+
+console.log(invertedString);
+
+// ченинг или цепочка вызовав
+const title = 'Top 10 react';
+const normalizesTitle = title.toLowerCase().split(' ').join('-');
+console.log(normalizesTitle);
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+let total2 = 0;
+
+const arra3 = array1.concat(array2, [10, 20]);
+console.log(arra3);
+
+for (const array of arra3) {
+  total2 += array;
+}
+console.log(total2);
+
+const cards = [
+  'Карточка-1',
+  'Карточка-2',
+  'Карточка-3',
+  'Карточка-4',
+  'Карточка-5',
+];
+
+// console.table(cards);
+const cardToRemove = 'Карточка-3';
+// const index1 = cards.indexOf(cardToRemove);
+// cards.splice(index1, 1);
+// console.table(cards);
+
+const cardToInsert = 'Обновленная карточка-4';
+const index3 = 3;
+// cards.push(cardToInsert);
+// cards.splice(index3, 0, cardToInsert);
+cards.splice(index3, 1, cardToInsert);
+console.log(cards);
