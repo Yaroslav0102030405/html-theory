14472c8879d66ea82c83ad3d9be956c72911626a
Yaroslav0102030405 <you@example.com>
2025-05-05 23:07:40 +0300

    js-23

--- js/js-23.js
+++ js/js-23.js
@@ -1,4 +1,56 @@
-// fetch('https://pokeapi.co/api/v2/pokemon/2')
-//   .then((resolve) => resolve.json())
-//   .then((pokemon) => console.log(pokemon))
-//   .catch((error) => console.log(error));
+// import pokemonCard from '../templates/pokemonCard';
+// console.log(pokemonCard);
+
+// import API from './api-service';
+
+const pokemonRef = document.querySelector('.pokemon');
+const formSearch = document.querySelector('.js-form-search');
+
+formSearch.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const searchQuery = e.currentTarget.elements.query.value;
+
+  fetchPokemon(searchQuery)
+    .then(onRenderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => formSearch.reset());
+}
+
+function fetchPokemon(pokemonId) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
+    (response) => {
+      return response.json();
+    }
+  );
+}
+
+function onRenderPokemonCard({ sprites, from_default, name, weight }) {
+  const markup = `<div class='card'>
+  <img class="center" src='${sprites.front_default}' alt='${name}' />
+  <div class='card-body'>
+    <h2>${name}</h2>
+    <p>${weight}</p>
+  </div>
+</div>`;
+  pokemonRef.innerHTML = markup;
+
+  //   return markup;
+}
+
+function onFetchError(error) {
+  console.log(error);
+  alert('Такого покемона ми не знайшли у нас в списку...');
+}
+
+fetchPokemon2().then((pokemon) => console.log(pokemon));
+
+function fetchPokemon2(pokemonId) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon?limit=5`).then((response) => {
+    return response.json();
+  });
+}
+
+console.log(fetchPokemon2);
